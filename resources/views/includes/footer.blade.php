e911eb49fdadb06c4406485ad5b58ee44b4ac30b
miarsiddique <miarsiddique@gmail.com>
2017-02-16 01:18:45 +0530

    admin config complete

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- To the right -->
+<div class="pull-right hidden-xs">
+	Anything you want
+</div>
+<!-- Default to the left -->
+<strong>Copyright &copy; 2017 <a href="#">By Irshad</a>.</strong> All rights reserved.
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <h1>Mohamad Irshad Alam</h1>
+@stop
